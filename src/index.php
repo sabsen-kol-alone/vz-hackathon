5b69bd60dea875e5cc8f043e6db2de36321541d8
sabyasachi <sabsen.kol@gmail.com>
2015-09-30 01:15:53 -0400

    Unknown uncommitted changes

--- src/index.php
+++ src/index.php
@@ -2,7 +2,12 @@
   error_reporting(-1);
   ini_set('display_errors', 1);
 
-  echo 'Hello World!<br>';
+  echo '
+    <html>
+    <head><title>Sample Page</title></head>
+    <body>';
+
+  echo '<h1>Sample Page Headline</h1>';
   
 //  phpinfo();
 
@@ -15,8 +20,13 @@
   $sample->set( 'Joe', 20);
   $sample->set( 'Che', 30);
 
-  echo '<pre>';
-  print_r( $sample->get_name(10));
-  print_r( $sample->get_name(30));
-  print_r( $sample->get_id('Joe'));
-  echo '</pre>';
+  $names = $sample->get_name(10);
+  echo "<p>ID: {$names[0]['id']} Name: {$names[0]['name']}</p>";
+
+  $names = $sample->get_name(30);
+  echo "<p>ID: {$names[0]['id']} Name: {$names[0]['name']}</p>";
+
+  $ids = $sample->get_id('Joe');
+  echo "<p>ID: {$ids[0]['id']}Name:  {$ids[0]['name']}</p>";
+
+  echo '</body></html>';
--- tests/features/test_web.feature
+++ tests/features/test_web.feature
@@ -3,3 +3,4 @@ Feature: Testing verizon.com
 @javascript
 Scenario: Testing basic web features
    Given I am on the homepage
+   Then I should see the text "ID: 10 Name: Tom"
